cccfed94c4f4e11546db15c7f135a501298e6d2b
Chungath Sabarish <sabarishchungath@riafy.me>
2021-08-30 13:05:27 +0530

    FB_SDK: integrated fb sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:facebook_app_events/facebook_app_events.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,6 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static final facebookAppEvents = FacebookAppEvents();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,15 +51,32 @@ class _MyHomePageState extends State<MyHomePage> {
                 RawMaterialButton(
                   fillColor: Colors.green,
                   onPressed: () {
-                    FirebaseAnalytics _analytics = FirebaseAnalytics();
-                    _analytics.logAddToCart(
-                        itemId: "121",
-                        itemName: "SHAMBU",
-                        itemCategory: "RIAFY_HQ",
-                        quantity: 1,
-                        currency: "AED");
+                    // FirebaseAnalytics _analytics = FirebaseAnalytics();
+                    // _analytics.logAddToCart(
+                    //     itemId: "121",
+                    //     itemName: "SHAMBU",
+                    //     itemCategory: "RIAFY_HQ",
+                    //     quantity: 1,
+                    //     currency: "AED");
                   },
                   child: Text('ADD_TO_CART',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w700)),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                RawMaterialButton(
+                  fillColor: Colors.green,
+                  onPressed: () {
+                    facebookAppEvents.logEvent(
+                      name: 'button_clicked',
+                      parameters: {
+                        'button_id': 'the_clickme_button',
+                      },
+                    );
+                  },
+                  child: Text('FACEBOOK',
                       style: TextStyle(
                           color: Colors.white, fontWeight: FontWeight.w700)),
                 )
